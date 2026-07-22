d84805e2e8dc27ac0d17cce3d533633618fddba9
amankumar89 <amankumaroo784@gmail.com>
2026-07-22 10:47:38 +0530

    initial spring boot backend setup

---
+++ backend/config-server/src/main/java/com/aman/nimbuscommerce/config/ConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.aman.nimbuscommerce.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServerApplication.class, args);
+	}
+
+}
---
+++ backend/payment-service/src/test/java/com/aman/nimbuscommerce/payment/PaymentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aman.nimbuscommerce.payment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
