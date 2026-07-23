07ffb1276fb06354b200bfc5b5ed0ca8d8729873
amankumar89 <amankumaroo784@gmail.com>
2026-07-23 21:10:52 +0530

    added global and custom exceptions

---
+++ backend/user-service/src/main/java/com/aman/nimbuscommerce/user/exception/DuplicateException.java
@@ -0,0 +1,7 @@
+package com.aman.nimbuscommerce.user.exception;
+
+public class DuplicateException extends RuntimeException{
+    public DuplicateException(String message){
+        super(message);
+    }
+}
