cdb24ff4458f49c7bd68a6465e33ea2cd69f310b
amankumar89 <amankumaroo784@gmail.com>
2026-07-29 10:46:32 +0530

    added product and category entity

---
+++ backend/product-service/src/main/java/com/aman/nimbuscommerce/product/entity/Category.java
@@ -0,0 +1,29 @@
+package com.aman.nimbuscommerce.product.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "categories")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(unique = true, nullable = false)
+    private String name;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "parent_category_id")
+    private Category parentCategory;
+}
