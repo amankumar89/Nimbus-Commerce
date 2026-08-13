0ed44c3d6f63492056c7333162838798a032e6bd
amankumar89 <amankumaroo784@gmail.com>
2026-08-13 11:46:07 +0530

    backend initial setup

--- backend/product-service/src/main/java/com/aman/nimbuscommerce/product/exception/NotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.aman.nimbuscommerce.product.exception;
-
-public class NotFoundException extends RuntimeException{
-    public NotFoundException(String message){
-        super(message);
-    }
-}
